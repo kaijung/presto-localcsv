048ed8e960553d7fce2cfc59822c81fe6e3952f4
kaijung_chen <kaijung_chen@arplanet.com.tw>
2019-11-07 22:50:08 +0800

    Fix import

--- src/main/java/com/facebook/presto/localcsv/LocalCsvHandleResolver.java
+++ src/main/java/com/facebook/presto/localcsv/LocalCsvHandleResolver.java
@@ -14,8 +14,12 @@
 package com.facebook.presto.localcsv;
 
 import com.facebook.presto.spi.ColumnHandle;
+import com.facebook.presto.spi.ConnectorHandleResolver;
+import com.facebook.presto.spi.ConnectorInsertTableHandle;
+import com.facebook.presto.spi.ConnectorOutputTableHandle;
 import com.facebook.presto.spi.ConnectorSplit;
 import com.facebook.presto.spi.ConnectorTableHandle;
+import com.facebook.presto.spi.ConnectorTableLayoutHandle;
 import com.facebook.presto.spi.connector.ConnectorTransactionHandle;
 
 public class LocalCsvHandleResolver
